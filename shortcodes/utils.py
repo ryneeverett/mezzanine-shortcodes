e664b491b336455c8b10c8d7e7e630790026739b
ryneeverett <ryneeverett@gmail.com>
2015-12-04 15:16:25 -0500

    Switch classproperty implementation.
    My implementation doesn't make much sense now that I know that a
    `property` can be overriden by a class attribute in a subclass. I find
    this behavior odd though, because you cannot depend on a `property` to
    behave the same as an instance attribute, which I thought was the whole
    point.

--- shortcodes/utils.py
+++ shortcodes/utils.py
@@ -1,12 +1,7 @@
 class classproperty(object):
-    """ Posted to http://stackoverflow.com/a/32289080/1938621. """
+    """ From http://stackoverflow.com/a/5192374/1938621. """
     def __init__(self, f):
         self.f = f
 
-    def __get__(self, instance, klass):
-        if instance:
-            try:
-                return self.f(instance)
-            except AttributeError:
-                pass
-        return self.f(klass)
+    def __get__(self, obj, owner):
+        return self.f(owner)
