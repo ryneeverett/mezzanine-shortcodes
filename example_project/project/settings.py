9b60764f4cab68094d3ba19da1e7152034f1ee15
ryneeverett <ryneeverett@gmail.com>
2015-12-04 13:04:13 -0500

    Fix example_project/tests.
    The static assets were ignored in the initial commit.

--- example_project/project/settings.py
+++ example_project/project/settings.py
@@ -184,7 +184,7 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), 'staticfiles')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
