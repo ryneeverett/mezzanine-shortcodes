f5073217ac0cb2061cce5085819fa2da8f927086
ryneeverett <ryneeverett@gmail.com>
2016-01-09 17:40:06 -0500

    Don't need to set the domain in development.
    I have no idea why but this no longer seems to be necessary.

--- shortcodes/apps.py
+++ shortcodes/apps.py
@@ -1,8 +1,6 @@
-import sys
 import importlib
 
 from django.apps import AppConfig, apps
-from django.conf import settings
 
 
 class ShortcodesConfig(AppConfig):
@@ -15,15 +13,3 @@ class ShortcodesConfig(AppConfig):
                 importlib.import_module('.'.join([app.name, 'shortcodes']))
             except ImportError:
                 pass
-
-        if settings.DEBUG:
-            from django.contrib.sites.models import Site
-
-            try:
-                addrport = sys.argv[-1] if sys.argv[-1] > 1024 else 8000
-            except TypeError:
-                addrport = 8000
-
-            site = Site.objects.get_current()
-            site.domain = '127.0.0.1:{port}'.format(port=addrport)
-            site.save()
