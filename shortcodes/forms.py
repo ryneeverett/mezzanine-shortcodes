ee746a5c4b179d2c49c940b750573d390daaebdd
ryneeverett <ryneeverett@gmail.com>
2016-07-11 21:23:06 -0400

    Fix OperationalError when running collectstatic.
    I run collectstatic as part of my build process and the database HOST
    doesn't exist when this module is imported.

--- shortcodes/forms.py
+++ shortcodes/forms.py
@@ -1,10 +1,10 @@
 from mezzanine.core import forms
 from django.contrib.sites.models import Site
-from django.db.utils import ProgrammingError
-
+from django.db.utils import OperationalError, ProgrammingError
 try:
     DOMAIN = Site.objects.get_current().domain
-except ProgrammingError:  # Database hasn't been initialized yet.
+except (OperationalError,   # Database doesn't exist.
+        ProgrammingError):  # Database hasn't been initialized yet.
     DOMAIN = ''
 
 
